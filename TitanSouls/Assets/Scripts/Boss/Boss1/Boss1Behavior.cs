2c0a397c41ee438e61819fd060f0b99e703bfb09
ParkDoil <103029337+ParkDoil@users.noreply.github.com>
2022-07-26 14:27:00 +0900

    [FEAT] Boss Movement 보스 양쪽 반복해서 움직이기 구현

--- TitanSouls/Assets/Scripts/Boss/Boss1/Boss1Behavior.cs
+++ TitanSouls/Assets/Scripts/Boss/Boss1/Boss1Behavior.cs
@@ -5,32 +5,70 @@ using UnityEngine;
 public class Boss1Behavior : MonoBehaviour
 {
     public float Speed;
-    public Sprite DieSprites;
     public GameObject WeakPoint;
+    public ParticleSystem HitEffect;
 
-    SpriteRenderer _spriteRenderer;
     Rigidbody2D _rigid;
+    Animator _animator;
+
+    private bool IsLeft;
+    private bool IsDead;
 
     void Awake()
     {
-        _spriteRenderer = GetComponent<SpriteRenderer>();
         _rigid = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        if (WeakPoint.GetComponent<WeakPoint>().IsHit)
-        {
-            _spriteRenderer.sprite = DieSprites;
+        _rigid.velocity = Vector2.zero;
 
-            Invoke("Die", 1f);
+        if (WeakPoint.GetComponent<WeakPoint>().IsHit == true)
+        {
+            IsDead = true;
+
+            _animator.SetTrigger(BossAnimID.DIE);
+
+            //HitEffect.transform.position = WeakPoint.transform.position;
+            //HitEffect.transform.rotation = WeakPoint.transform.rotation;
+            //HitEffect.Play();
+
+            Invoke("Die", 1.5f);
+        }
+
+        if (IsDead == false)
+        {
+             Move();
+        }
+    }
+
+    void Move()
+    {
+        Vector2 MoveVec = Vector2.right * Speed;
+        if (IsLeft == true)
+        {
+            _rigid.AddForce(-MoveVec);
+
+            if (transform.position.x <= -16f)
+            {
+                IsLeft = false;
+            }
+        }
+        else
+        {
+            _rigid.AddForce(MoveVec);
+
+            if(transform.position.x >= 7f)
+            {
+                IsLeft = true;
+            }
         }
     }
 
     void Die()
     {
-        gameObject.SetActive(false);
-
+        Destroy(gameObject);
     }
 
 }
---
+++ TitanSouls/Assets/Scripts/Util/AnimID.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public static class PlayerAnimID
+{
+
+}
+
+public static class BossAnimID
+{
+    public static readonly int DIE = Animator.StringToHash("IsDie");
+}
